537b715f153a0c6f99350f55486d8901289b35e6
Nat-1501 <natalisoares90@gmail.com>
2022-10-06 17:36:38 -0300

    criando janela

---
+++ main.py
@@ -0,0 +1,21 @@
+import tkinter
+from tkinter import *
+from tkinter import ttk 
+
+# cores --------------------------------
+co0 = "#FFFFFF"  # white / branca
+co1 = "#333333"  # black / preta
+co2 = "#fcc058"  # orange / laranja
+co3 = "#fff873"  # yellow / amarela
+co4 = "#34eb3d"   # green / verde
+co5 = "#e85151"   # red / vermelha
+
+fundo = "#3b3b3b"
+
+# comfigurando a janela
+janela = Tk()
+janela.title('')
+janela.geometry('260x280')
+janela.configure(bg=fundo)
+
+janela.mainloop()
